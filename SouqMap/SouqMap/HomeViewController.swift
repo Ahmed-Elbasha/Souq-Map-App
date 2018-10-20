2f7fddb0ad6de67c37487adc5fc35e5f55655ba0
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-10-20 15:20:31 +0200

    Added UI Design to HomeViewController

---
+++ SouqMap/SouqMap/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  SouqMap
+//
+//  Created by Ahmed Elbasha on 10/20/18.
+//  Copyright © 2018 Ahmed Elbasha. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
